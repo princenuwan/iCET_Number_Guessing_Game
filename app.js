f2e27bc0863f59582d465b02dc0d97effa2a3978
princenuwan <nuwanuk3@gmail.com>
2025-07-13 22:23:26 +0530

    button naviagtions linked through pages

--- app.js
+++ app.js
@@ -1,2 +1,18 @@
 console.log(Math.floor(Math.random()*4));
 
+function clickEasyBtn(){
+    window.location.href = "easy.html";
+}
+
+function clickMediumBtn(){
+    window.location.href = "medium.html";
+}
+
+function clickHardBtn(){
+    window.location.href = "hard.html";
+}
+
+function clickHome(){
+    window.location.href = "index.html";
+}
+
