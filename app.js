463b3c74a8e54cacaddfcc522ecd42c4f17d757d
princenuwan <nuwanuk3@gmail.com>
2025-07-13 23:22:59 +0530

    update all paths

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 console.log(Math.floor(Math.random()*4));
 
 function clickEasyBtn(){
-    window.location.href = "./easy.html";
+    window.location.href = "/iCET_Number_Guessing_Game/easy.html";
 }
 
 function clickMediumBtn(){
-    window.location.href = "./medium.html";
+    window.location.href = "/iCET_Number_Guessing_Game/medium.html";
 }
 
 function clickHardBtn(){
-    window.location.href = "./hard.html";
+    window.location.href = "/iCET_Number_Guessing_Game/hard.html";
 }
 
 function clickHome(){
-    window.location.href = "./index.html";
+    window.location.href = "/iCET_Number_Guessing_Game/index.html";
 }
 
