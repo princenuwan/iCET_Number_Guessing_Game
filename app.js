e3814d1a8ec1b6d0e59681b331038cbf59364972
princenuwan <nuwanuk3@gmail.com>
2025-07-13 23:46:53 +0530

    update

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 console.log(Math.floor(Math.random()*4));
 
 function clickEasyBtn(){
-    window.location.href = "https://princenuwan.github.io/iCET_Number_Guessing_Game/easy.html";
+    window.location.href = "iCET_Number_Guessing_Game/easy.html";
 }
 
 function clickMediumBtn(){
-    window.location.href = "https://princenuwan.github.io/iCET_Number_Guessing_Game/medium.html";
+    window.location.href = "iCET_Number_Guessing_Game/medium.html";
 }
 
 function clickHardBtn(){
-    window.location.href = "https://princenuwan.github.io/iCET_Number_Guessing_Game/hard.html";
+    window.location.href = "iCET_Number_Guessing_Game/hard.html";
 }
 
 function clickHome(){
-    window.location.href = "https://princenuwan.github.io/iCET_Number_Guessing_Game/index.html";
+    window.location.href = "iCET_Number_Guessing_Game/index.html";
 }
