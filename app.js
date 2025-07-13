3e89772b8900963baa2871ab82c49bba05412353
princenuwan <nuwanuk3@gmail.com>
2025-07-13 22:34:01 +0530

    buttons not working solved that root file path ./

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 console.log(Math.floor(Math.random()*4));
 
 function clickEasyBtn(){
-    window.location.href = "easy.html";
+    window.location.href = "./easy.html";
 }
 
 function clickMediumBtn(){
-    window.location.href = "medium.html";
+    window.location.href = "./medium.html";
 }
 
 function clickHardBtn(){
-    window.location.href = "hard.html";
+    window.location.href = "./hard.html";
 }
 
 function clickHome(){
-    window.location.href = "index.html";
+    window.location.href = "./index.html";
 }
 
