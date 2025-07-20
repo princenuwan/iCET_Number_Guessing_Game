f6eec4cbb9ecb2e3a1956c975836203b893bc696
princenuwan <nuwanuk3@gmail.com>
2025-07-20 07:00:12 +0530

    page navigatio issue fixed

--- app.js
+++ app.js
@@ -1,17 +1,2 @@
 console.log(Math.floor(Math.random()*4));
 
-function clickEasyBtn(){
-    window.location.href = "iCET_Number_Guessing_Game/easy.html";
-}
-
-function clickMediumBtn(){
-    window.location.href = "iCET_Number_Guessing_Game/medium.html";
-}
-
-function clickHardBtn(){
-    window.location.href = "iCET_Number_Guessing_Game/hard.html";
-}
-
-function clickHome(){
-    window.location.href = "iCET_Number_Guessing_Game/index.html";
-}
